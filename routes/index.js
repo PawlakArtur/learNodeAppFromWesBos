a6a3f682aea4ad668a4415769570b3f8ad8f2ef4
Artur Pawlak <pawlak.artur90@gmail.com>
2019-02-23 17:35:19 +0100

    Add init files

---
+++ routes/index.js
@@ -0,0 +1,10 @@
+const express = require('express');
+const router = express.Router();
+
+// Do work here
+router.get('/', (req, res) => {
+  const wes = { name: 'Wes', age: 100, cool: true };
+  res.json(wes);
+});
+
+module.exports = router;
