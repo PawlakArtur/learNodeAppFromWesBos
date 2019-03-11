92a9d7e57617136af4f5318b0961d0139f1b52bf
Artur Pawlak <pawlak.artur90@gmail.com>
2019-03-11 18:53:55 +0100

    Add new views for login and register users

--- models/User.js
+++ models/User.js
@@ -22,7 +22,7 @@ const userSchema = new Schema({
 	}
 });
 
-userSchema.plugin(passportLocalMongoose, { usernameFields: 'email' });
+userSchema.plugin(passportLocalMongoose, { usernameField: 'email' });
 userSchema.plugin(mongodbErrorHandler);
 
 module.exports = mongoose.model('User', userSchema);
