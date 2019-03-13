4dc23fedf39b6ab65bfc6401790113e33d518076
Artur Pawlak <pawlak.artur90@gmail.com>
2019-03-13 21:22:24 +0100

    Protect route '/add' from unauthorized users

--- controllers/authController.js
+++ controllers/authController.js
@@ -12,3 +12,13 @@ exports.logout = (req, res) => {
 	req.flash('success', 'You are logged out');
 	res.redirect('/');
 }
+
+exports.isLoggedIn = (req, res, next) => {
+	// Check if the user is authenticated
+	if(req.isAuthenticated()) {
+		next();
+		return;
+	}
+	req.flash('error', 'You must be logg in');
+	res.redirect('/');
+}
