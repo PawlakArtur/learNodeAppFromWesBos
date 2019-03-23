2c71baecf726ce32c5d3f7246136b747aae2d4a6
Artur Pawlak <pawlak.artur90@gmail.com>
2019-03-23 19:50:55 +0100

    Handle display reviews in store view

--- models/Store.js
+++ models/Store.js
@@ -39,6 +39,13 @@ const storeSchema = new mongoose.Schema({
 		ref: 'User',
 		required: 'You must supply an athor'
 	}
+}, {
+	toJSON: {
+		virtuals: true
+	},
+	toObject: {
+		virtuals: true
+	}
 });
 
 // Define our indexes
@@ -74,5 +81,11 @@ storeSchema.statics.getTagsList = function() {
 		{ $sort: { count: -1 }}
 	]);
 }
+
+storeSchema.virtual('reviews', {
+	ref: 'Review', // what model to link
+	localField: '_id', // which field on the store
+	foreignField: 'store' // which field on the review
+});
 
 module.exports = mongoose.model('Store', storeSchema);
