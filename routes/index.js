4bb38b31a9f042f86be51c059998605a765418fa
Artur Pawlak <pawlak.artur90@gmail.com>
2019-03-23 17:16:44 +0100

    Handle adding new reviews for stores

--- routes/index.js
+++ routes/index.js
@@ -3,6 +3,7 @@ const router = express.Router();
 const storeController = require('../controllers/storeController');
 const userController = require('../controllers/userController');
 const authController = require('../controllers/authController');
+const reviewController = require('../controllers/reviewController');
 const { catchErrors } = require('../handlers/errorHandlers');
 
 // Do work here
@@ -46,6 +47,7 @@ router.post('/account/reset/:token',
 );
 router.get('/map', storeController.mapPage);
 router.get('/hearts', authController.isLoggedIn, catchErrors(storeController.getHearts));
+router.post('/review/:id', authController.isLoggedIn, catchErrors(reviewController.addReview));
 
 // API
 
